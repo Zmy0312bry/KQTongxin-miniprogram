b5c11bd2c076f26e7ba67983cbaf5831f0a4eabc
Torch_byr <zhaomingyuan@bupt.edu.cn>
2026-03-19 14:14:31 +0800

    fix bug

--- src/app.js
+++ src/app.js
@@ -1,34 +1,67 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue' // ✅ 确保存在 App.vue
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 // NutUI 组件按需引入
 import {
-  Swiper, SwiperItem, Button, Cell, CellGroup, Row, Col,
-  Noticebar, Divider, Form, FormItem, Radio, RadioGroup,
-  Tabbar, TabbarItem, List, Avatar, Input, Textarea, Tabs,Rate,
-  TabPane, Toast, Skeleton, ImagePreview, Popup, Grid, GridItem
-} from '@nutui/nutui-taro'
+  Swiper,
+  SwiperItem,
+  Button,
+  Cell,
+  CellGroup,
+  Row,
+  Col,
+  Noticebar,
+  Divider,
+  Form,
+  FormItem,
+  Radio,
+  RadioGroup,
+  Tabbar,
+  TabbarItem,
+  List,
+  Avatar,
+  Input,
+  Textarea,
+  Tabs,
+  Rate,
+  TabPane,
+  Toast,
+  Skeleton,
+  ImagePreview,
+  Popup,
+  Grid,
+  GridItem,
+} from "@nutui/nutui-taro";
 
-import './app.css'
-import "@nutui/nutui-taro/dist/style.css"
+import "./app.css";
+import "@nutui/nutui-taro/dist/style.css";
 
-const app = createApp(App)
+const App = createApp({
+  onShow(options) {
+    // console.log('App onShow.')
+  },
+  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
+});
 
 // ✅ 注入 Pinia
-app.use(createPinia())
+App.use(createPinia());
 
 // ✅ 注册组件
-app
-  .use(Swiper).use(SwiperItem)
+App.use(Swiper)
+  .use(SwiperItem)
   .use(Button)
-  .use(Cell).use(CellGroup)
-  .use(Row).use(Col)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Row)
+  .use(Col)
   .use(Noticebar)
   .use(Divider)
-  .use(Form).use(FormItem)
-  .use(Radio).use(RadioGroup)
-  .use(Tabbar).use(TabbarItem)
+  .use(Form)
+  .use(FormItem)
+  .use(Radio)
+  .use(RadioGroup)
+  .use(Tabbar)
+  .use(TabbarItem)
   .use(List)
   .use(Avatar)
   .use(Input)
@@ -41,6 +74,6 @@ app
   .use(Grid)
   .use(GridItem)
   .use(Skeleton)
-  .use(Rate)
+  .use(Rate);
 
-export default app
+export default App;
